fc82373aeb053326f547d10b7ef483cf028b541e
Ilya.Kolotov <kolotov.iljya@gmail.com>
2022-12-20 16:40:51 +0500

    Add S3 Task 4

---
+++ S3_Task4.py
@@ -0,0 +1,27 @@
+# Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+# Пример:
+# - 45 -> 101101
+# - 3 -> 11
+# - 2 -> 10
+
+def InputNumbers(InputText):
+    is_OK = False
+    while not is_OK:
+        try:
+            number = int(input(f'{InputText}'))
+            is_OK = True
+        except ValueError:
+            print('Ошибка! Введите число')
+    return number
+def dec_to_bin(number):
+    binNumber = 0
+    d10 = 1
+    while (number > 0):
+        binNumber = binNumber + number % 2 * d10
+        number //= 2
+        d10 *= 10
+    return binNumber
+
+num = InputNumbers('Введите число: ')
+binNumber = dec_to_bin(num)
+print(binNumber)
