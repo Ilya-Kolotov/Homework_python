ef3e4435b5a1abc93d8e5065b6825e6850a10b16
Ilya.Kolotov <kolotov.iljya@gmail.com>
2022-12-26 12:45:38 +0500

    Add S4 Task1

---
+++ S4_Task1.py
@@ -0,0 +1,18 @@
+# Вычислить число c заданной точностью d
+# Пример:
+# - при d = 0.001, π = 3.141   10^{-1} ≤ d ≤10^{-10}
+import math
+def InputNumbers(InputText):
+    is_OK = False
+    while not is_OK:
+        try:
+            number = int(input(f'{InputText}'))
+            if number >= 0:
+                is_OK = True
+            else:
+                print('Ошибка! Введите неотрицательное число')
+        except ValueError:
+            print('Ошибка! Введите число')
+    return number
+d = InputNumbers('Введите количество знаков после запятой: ')
+print(f'При заданной точности d = {(10 ** -d):{d}f}, число пи = {round(math.pi, d)}')
